cee5d326829dc0e8dcf2a80e4485eb11181e9aa8
clbokea <clbo@kea.dk>
2016-12-06 10:05:23 +0100

    mongodbadded

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@
 //mongodb://clbokea:1234@ds053964.mlab.com:53964/store_2
 
 module.exports = {
-    mongodb : 'mongodb://localhost:27017/store'
+    mongodb : 'mongodb://clbo:1234@ds127428.mlab.com:27428/clboherukoadv'
 }
 
